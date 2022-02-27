64027da3bf768c2dcf7d0b6e613f7e826ad46c1c
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-02-27 16:48:37 +0330

    feat: import page ui

---
+++ src/components/Layout/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="mt-2">
+      <p className="text-center text-white text-slate-400 font-thin">Developed with love by Ahmad Karimzade</p>
+    </div>
+  )
+}
--- src/pages/Import/index.jsx
+++ src/pages/Import/index.jsx
@@ -1,7 +1,19 @@
+import DefaultLayout from '../../layouts/default'
+
 export default function ImportPage() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-800 font-serif">
-      Hello world!
-    </h1>
+    <DefaultLayout>
+      <div className="container min-h-[95vh] mx-auto flex flex-col justify-center items-center">
+        <div className="text-white text-center">
+          <h1 className="text-7xl font-mono">icom</h1>
+          <p className="font-thin">Create icon components using svg</p>
+        </div>
+
+        <div className="mt-5">
+          <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create new pack</button>
+          <button class="ml-2 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-700 focus:white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Import</button>
+        </div>
+      </div>
+    </DefaultLayout>
   )
 }
