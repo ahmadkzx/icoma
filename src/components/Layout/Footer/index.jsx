3c6049852c145f52c4fd035ee5bef71846c3bd6b
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-02-28 14:26:22 +0330

    refactor: remove landing

--- src/components/Layout/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="mt-2">
-      <p className="text-center text-white text-slate-400 font-thin">Developed with love by Ahmad Karimzade</p>
-    </div>
-  )
-}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
