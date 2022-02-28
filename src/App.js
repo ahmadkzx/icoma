b9977d4ea465460615f8cb192dd132002c46b846
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-02-28 11:51:22 +0330

    feat: add react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './assets/styles/app.scss'
 import LandingPage from './pages/Landing'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} exact />
+      </Routes>
     </div>
   )
 }
