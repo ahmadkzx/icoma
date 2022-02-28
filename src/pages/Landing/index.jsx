49435746e4a20d849045b1e199d17f393e4b80a3
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-02-28 13:11:59 +0330

    feat: import input

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -11,11 +11,20 @@ export default function LandingPage() {
         </div>
 
         <div className="mt-5">
-          <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" data-modal-toggle="create-pack-modal">Create new pack</button>
-          <button className="ml-3 text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700">Import</button>        </div>
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            data-modal-toggle="create-pack-modal"
+          >
+            Create new pack
+          </button>
+          <label for="import" className="cursor-pointer ml-3 text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700">
+            Import
+            <input type="file" id="import" className="invisible w-0" />
+          </label>
+        </div>
       </div>
 
       <CreatePackModal />
     </DefaultLayout>
-  )
+  );
 }
