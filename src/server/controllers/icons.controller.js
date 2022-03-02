03ce5c617445dfca2c6659d3faaa4482280e8436
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-03-02 20:45:24 +0330

    refactor: use single json file for selections

--- src/server/controllers/icons.controller.js
+++ src/server/controllers/icons.controller.js
@@ -23,16 +23,10 @@ export async function addIcon(req, res) {
 
 export async function getIcons(req, res) {
   try {
-    const src = path.join(__dirname, '../../../mock/icons')
-    const filesName = await fs.readdir(src)
+    const src = path.join(__dirname, '../../../mock/icons.json')
+    const icons = await fs.readFile(src, { encoding: 'utf-8' })
 
-    const files = await Promise.all(filesName.map(async name => {
-      const filePath = path.join(src, name)
-      const file = await fs.readFile(filePath, { encoding: 'utf-8' })
-      return JSON.parse(file)
-    }))
-
-    res.status(200).json({ data: files })
+    res.status(200).json({ data: JSON.parse(icons) })
 
   } catch(err) {
     console.error('[SERVER ERROR]: ', err)
