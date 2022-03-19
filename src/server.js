80ec93eda553b52c8b3b165fd55836543ac2a206
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-03-19 23:22:49 +0330

    feat: config server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import cors from 'cors'
+import express from 'express'
+import routes from './api/routes'
+
+(async function startIcom() {
+  try {
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    app.use('/api', routes)
+    if (process.env.NODE_ENV == 'production') app.use('/app', express.static(__dirname))
+    await app.listen(5000)
+
+    console.log('Icom started...')
+
+  } catch(err) {
+    console.error(err)
+  }
+})()
--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,75 @@ const dotenv = require("dotenv");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
-
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: "index.bundle.js",
-  },
-
-  mode: process.env.NODE_ENV || "development",
-
-  devServer: {
-    port: 8000,
-  },
-
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
+module.exports = (env, argv) => {
+  const UI_CONFIG = {
+    entry: path.join(__dirname, "src", "index.js"),
+  
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "ui.icom.js",
+    },
+  
+    devServer: {
+      port: 8000,
+    },
+  
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+      ],
+    },
+  
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: path.join(__dirname, "src", "index.html"),
+      }),
+  
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(dotenv.config({ path: "./.env" }).parsed)
+      })
     ],
-  },
+  }
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-    }),
+  const SERVER_CONFIG = {
+    target: "node",
 
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config({ path: "./.env" }).parsed)
-    })
-  ],
+    entry: path.join(__dirname, "src", "server.js"),
+
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "server.icom.js",
+    },
+
+    resolve: {
+      extensions: [".js"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+        }
+      ],
+    },
+  }
+  
+  return (argv.mode == "development")
+    ? UI_CONFIG
+    : [SERVER_CONFIG, UI_CONFIG]
 };
