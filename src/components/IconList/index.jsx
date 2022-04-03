7832a4f1f611511486a5aafa2c8e3e354a7a4ee9
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-04-03 17:38:57 +0430

    fix: icon list columns

--- src/components/IconList/index.jsx
+++ src/components/IconList/index.jsx
@@ -3,7 +3,7 @@ import IconModal from '../IconModal'
 
 export default function IconList({ icons }) {
   return (
-    <div className="grid grid-cols-18 gap-2">
+    <div className="grid md:grid-cols-10 lg:grid-cols-12 xl:grid-cols-16 2xl:grid-cols-18 gap-2">
       {icons.map((icon, index) => <IconCard icon={icon} key={icon.id} />)}
       <IconModal />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateColumns: {
-        18: 'repeat(18, minmax(0, 1fr))'
+        18: 'repeat(18, minmax(0, 1fr))',
+        16: 'repeat(16, minmax(0, 1fr))'
       }
     }
   },
